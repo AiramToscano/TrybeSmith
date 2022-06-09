c90dcaccde07c9a471a27e18b4c7eb0b30637515
Airam Toscano <airamlobato@gmail.com>
2022-06-09 16:16:08 -0400

    Feat: try/catch

--- src/controllers/productscontroller.ts
+++ src/controllers/productscontroller.ts
@@ -4,8 +4,12 @@ import productsservice from '../services/productsservices';
 const STATUS_CODE_OK = 200;
 
 async function getAll(_req: Request, res: Response) {
-  const products = await productsservice.getAll();
-  return res.status(STATUS_CODE_OK).json(products);
+  try {
+    const products = await productsservice.getAll();
+    return res.status(STATUS_CODE_OK).json(products);
+  } catch (err) {
+    return res.status(500).json({ message: 'nenhum produto no banco' });
+  }
 }
 
 export default {

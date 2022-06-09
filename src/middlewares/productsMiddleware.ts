38aa120fb70f0e654d6e498e52f15673eb8f7b29
Airam Toscano <airamlobato@gmail.com>
2022-06-09 18:23:13 -0400

    Feat: add middleware amount  required

--- src/middlewares/productsMiddleware.ts
+++ src/middlewares/productsMiddleware.ts
@@ -12,6 +12,20 @@ function validateName(req: Request, res: Response, next: NextFunction) {
   next();
 }
 
+function validateAmount(req: Request, res: Response, next: NextFunction) {
+  const { amount } = req.body;
+  if (!amount) return res.status(404).json({ message: '"amount" is required' });
+  if (typeof (amount) !== 'string') {
+    return res.status(404).json({ message: '"amount" must be a string' });
+  }
+  if (amount.length < 2) {
+    return res.status(404)
+      .json({ message: '"amount" length must be at least 3 characters long' });
+  }
+  next();
+}
+
 export default {
   validateName,
+  validateAmount,
 };

4e4854a43207ead27db67f5d742d08fe22a55a5a
Airam Toscano <airamlobato@gmail.com>
2022-06-09 17:59:01 -0400

    Feat: add createproduct in controllers

--- src/controllers/productscontroller.ts
+++ src/controllers/productscontroller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import productsservice from '../services/productsservices';
 
 const STATUS_CODE_OK = 200;
+const STATUS_CODE_CREATED = 201;
 
 async function getAll(_req: Request, res: Response) {
   try {
@@ -12,6 +13,17 @@ async function getAll(_req: Request, res: Response) {
   }
 }
 
+async function createProduct(req: Request, res: Response) {
+  try {
+    const { name, amount } = req.body;
+    const user = await productsservice.createProduct(name, amount);
+    return res.status(STATUS_CODE_CREATED).json(user);
+  } catch (err) {
+    return res.status(500).json({ message: 'Não foi possivel criar o produto' });
+  }
+}
+
 export default {
   getAll,
+  createProduct,
 };

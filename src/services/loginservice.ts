67edb429362df851ec90d844b8da2e5a8cffb443
Airam Toscano <airamlobato@gmail.com>
2022-06-10 18:18:59 -0400

    Feat: add req5

--- src/services/loginservice.ts
+++ src/services/loginservice.ts
@@ -3,7 +3,7 @@ import createtoken from '../utils/createJWT';
 
 async function getAll(username: string, password: string) {
   const data = await loginmodel.getAll(username, password);
-  if (!data) return false;
+  if (data.length <= 0) return false;
   const createToken = createtoken.createJWT(username);
   return createToken;
 }

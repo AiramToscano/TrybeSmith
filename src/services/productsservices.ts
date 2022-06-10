34cca96025b00676454e7bd9b71f2c1cb3fdea30
Airam Toscano <airamlobato@gmail.com>
2022-06-10 17:42:35 -0400

    Feat: create logininterfaces.ts

--- src/services/productsservices.ts
+++ src/services/productsservices.ts
@@ -5,9 +5,19 @@ async function getAll() {
   return data;
 }
 
+// const objErrorExists = {
+//   error: 409,
+//   message: 'Product already exists',
+// };
+
+// class objErrorExists extends Error {
+  
+// };
+
 async function createProduct(name: string, amount: string) {
   const data = await productsmodel.createProduct(name, amount);
   // console.log(data);
+  if (!data) throw new Error('Não foi possivel fazer o cadastro do produto');
   return data;
 }
 
